d64a5174a16662bf9a7a45b6228b27a318ad7367
MichelleOSW <109711201+MichelleOSW@users.noreply.github.com>
2022-07-23 21:26:31 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -25,7 +25,6 @@ def user_input_features():
             'petal_width': petal_width}
     features = pd.DataFrame(data, index=[0])
     
-    if st.sidebar.button('Check Result'),
     return features
 
 df = user_input_features()
