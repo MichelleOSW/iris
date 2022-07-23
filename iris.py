fbd398bade675cf42f47ee1f27506a6887e5f935
MichelleOSW <109711201+MichelleOSW@users.noreply.github.com>
2022-07-23 21:40:50 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -45,7 +45,9 @@ prediction_proba = clf.predict_proba(df)
 
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+df1 = (['Iris-setosa','Iris-versicolor','Iris-virginica']) 
+st.dataframe(df1)
+
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
