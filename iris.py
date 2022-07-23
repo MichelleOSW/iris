dafc5e745d699497d705a631e2b981322195f244
MichelleOSW <109711201+MichelleOSW@users.noreply.github.com>
2022-07-23 20:33:18 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -42,8 +42,8 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-column = (['Class Label'])
-df1 = (['Iris-setosa','Iris-versicolor','Iris-virginica'],(column)) 
+column name = (['Class Label'])
+df1 = (['Iris-setosa','Iris-versicolor','Iris-virginica'],{column name}) 
 st.table(df1)
 
 st.subheader('Prediction')
